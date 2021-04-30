b1e6e14774d52722b15ae23be4966ab4256249a4
박우선 <zoe0526@naver.com>
2021-04-30 18:11:55 +0900

    caterpillar 애니메이션

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float move_speed = 0f;
+    float vertical = 0; 
+    private void Awake()
+    {  
+    }
+    enum character_anim
+    {
+        move,
+        run,
+        dance,
+        die
+    }
+    public void character_move()
+    { 
+        move_speed = 20f;
+
+        transform.position += Vector3.right *move_speed*Time.deltaTime;
+        transform.gameObject.GetComponent<Animator>().Play(character_anim.move.ToString());
+    }
+    public void character_run()
+    { 
+        move_speed = 15f;
+
+        transform.position += Vector3.right * move_speed * Time.deltaTime;
+        transform.gameObject.GetComponent<Animator>().Play(character_anim.run.ToString());
+    }
+    public void Update()
+    {
+        character_move();
+    }
+}
